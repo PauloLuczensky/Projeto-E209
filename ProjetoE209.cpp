6ad6d44874d16f5c77a0abb83169718e9d73ac08
PauloLuczensky <99833304+PauloLuczensky@users.noreply.github.com>
2022-06-19 18:13:00 -0300

    Update ProjetoE209.cpp

--- ProjetoE209.cpp
+++ ProjetoE209.cpp
@@ -18,7 +18,7 @@ void UART_Init(unsigned int ubrr);
 void UART_Transmit(char *dados);
 
 ISR(INT0_vect){
-  //VErificar esse if, ver se n„o È igual ao da main!!!
+  //VErificar esse if, ver se n√£o √© igual ao da main!!!
   if(TCCR2B = 0b00001000){
      TCCR2B = 0b00000111;
   }
@@ -42,14 +42,14 @@ ISR(INT1_vect){
 
 ISR(TIMER2_OVF_vect){
   
-//Caso o bot„o fique pressionado atÈ 5 segundos  
+//Caso o bot√£o fique pressionado at√© 5 segundos  
 if(cont >= 305 && sistema == false){
-  PORTD |= (1<<PD4);//Acendendo LED, atÈ atingir 5 segundos
+  PORTD |= (1<<PD4);//Acendendo LED, at√© atingir 5 segundos
   sistema = true;
   cont = 0;
 } 
 
-//ApÛs 5 segundos do bot„o pressionado, o LED acender· por 4 segundos
+//Ap√≥s 5 segundos do bot√£o pressionado, o LED acender√° por 4 segundos
 if(cont >= 244 && sistema == true && aux == false){
   PORTD ^= (1 << PD4); //Desligando o LED que mostra que o sistema foi ligado 
   UART_Transmit("Sistema ligado");//indicando que o sistema foi ligado
@@ -60,10 +60,10 @@ if(cont >= 244 && sistema == true && aux == false){
   EIMSK &= ~(1<<PD2);//impede que o sistema reinicie 
 } 
 
-//Passados os 25 segundos, o motor deve permanecer em rotaÁ„o com metade de sua velocidade m·xima.
+//Passados os 25 segundos, o motor deve permanecer em rota√ß√£o com metade de sua velocidade m√°xima.
 if(cont >= 1526 && sistema == true && aux == true){
-    ADCSRA |= (1<<ADEN);//ativando a convers„o
-    ADCSRA |= (1<<ADSC);//Inicializando a convers„o
+    ADCSRA |= (1<<ADEN);//ativando a convers√£o
+    ADCSRA |= (1<<ADSC);//Inicializando a convers√£o
     TCCR0B = 0b00000000;//desativando o Timer
     ADCSRA &= ~(1<<ADEN);
     //PWM igual a 127, metade do valor
@@ -76,28 +76,28 @@ if(cont >= 1526 && sistema == true && aux == true){
 void setup()
 {
   //VER O POTENCIOMETRO, PARA ANALISAR O PWM
-  UART_Init(MYUBRR);//Inicializa a comunicaÁ„o serial
-  DDRD = 0b00110000; //PD5 saÌda (motor da esteira), PD4 saÌda(LED que mostrar· "Sistema Ligado"), PD3 entrada(bot„o que desligar· o sistema), PD2 entrada(bot„o que ligar· o sistema)
-  DDRC = 0b00000000;//Usando o A0, ser· entrada. Variaremos para analisar o PWM
-  PORTD = 0b0000110;//Definindo pull up, nos botıes PD3 e PD2
+  UART_Init(MYUBRR);//Inicializa a comunica√ß√£o serial
+  DDRD = 0b00110000; //PD5 sa√≠da (motor da esteira), PD4 sa√≠da(LED que mostrar√° "Sistema Ligado"), PD3 entrada(bot√£o que desligar√° o sistema), PD2 entrada(bot√£o que ligar√° o sistema)
+  DDRC = 0b00000000;//Usando o A0, ser√° entrada. Variaremos para analisar o PWM
+  PORTD = 0b0000110;//Definindo pull up, nos bot√µes PD3 e PD2
   TCNT2 = 0;//inicializando o contador em 0
-  TIMSK2 = 0b00000001;// Ativando a interrupÁ„o por overflow
+  TIMSK2 = 0b00000001;// Ativando a interrup√ß√£o por overflow
   TCCR2A = 0b00000011;//fast PWM to OCR0A
-  TCCR2B = 0b0000000;// sem pre-escaler, iniciar· o sistema
+  TCCR2B = 0b00001000;// sem pre-escaler, iniciar√° o sistema
   OCR2A = 0;
-  EIMSK = (1<<INT1) | (1<<INT0); //InterrupÁ„o do PD2 e do PD3
-  EICRA |= ((0<<ISC01) | (1<<ISC00)); //TransiÁ„o de subida e descida, configurando o PD2
-  EICRA |= ((0<<ISC11) | (1<<ISC10)); //TransiÁ„o de subida e descida, configurando o PD3
-  sei();//serviÁo gobal de interrupÁ„o ativado  
+  EIMSK = (1<<INT1) | (1<<INT0); //Interrup√ß√£o do PD2 e do PD3
+  EICRA |= ((0<<ISC01) | (1<<ISC00)); //Transi√ß√£o de subida e descida, configurando o PD2
+  EICRA |= ((0<<ISC11) | (1<<ISC10)); //Transi√ß√£o de subida e descida, configurando o PD3
+  sei();//servi√ßo gobal de interrup√ß√£o ativado  
 }
 
 void loop()
 {  
-    //Analisando a variaÁ„o do PWM
+    //Analisando a varia√ß√£o do PWM
     while(ADCSRA == (ADCSRA |(1 << ADSC)));
     OCR2A = (ADC*255)/1023;
     
-    //Caso se digite 'P', o mesmo mostrar· a velocidade de rotaÁ„o atual do motor
+    //Caso se digite 'P', o mesmo mostrar√° a velocidade de rota√ß√£o atual do motor
     if(msg_rx[0] == 'P' && sistema == true){ 
       velocidade = (1800/255)*OCR2A;
       itoa(velocidade,msg_rx,10);
@@ -105,13 +105,13 @@ void loop()
       UART_Transmit(msg_rx);
       UART_Transmit(" rpm");
       UART_Transmit("\n");
-      msg_rx[0] == 'K';//condiÁ„o para n„o retornar
+      msg_rx[0] == 'K';//condi√ß√£o para n√£o retornar
     }
 }
 
 ISR(USART_RX_vect)
 {
-  // Escreve o valor recebido pela UART na posiÁ„o pos_msg_rx do buffer msg_rx
+  // Escreve o valor recebido pela UART na posi√ß√£o pos_msg_rx do buffer msg_rx
   msg_rx[pos_msg_rx++] = UDR0;
   if (pos_msg_rx == tamanho_msg_rx)
     pos_msg_rx = 0;
@@ -122,10 +122,10 @@ void UART_Transmit(char * dados)
   // Envia todos os caracteres do buffer dados ate chegar um final de linha
   while (*dados != 0)
   {
-    while (!(UCSR0A & (1 << UDRE0))); // Aguarda a transmiss„o acabar
-    // Escreve o caractere no registro de tranmiss„o
+    while (!(UCSR0A & (1 << UDRE0))); // Aguarda a transmiss√£o acabar
+    // Escreve o caractere no registro de tranmiss√£o
     UDR0 = *dados;
-    // Passa para o prÛximo caractere do buffer dados
+    // Passa para o pr√≥ximo caractere do buffer dados
     dados++;
   }
 }
